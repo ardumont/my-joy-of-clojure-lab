0a05a9f30335f2f54f318f813d63929411c45a5b
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2011-12-07 20:31:15 +0100

    map-bit-xor

--- src/my_joy_of_clojure_lab/core.clj
+++ src/my_joy_of_clojure_lab/core.clj
@@ -1,7 +1,16 @@
 (ns my-joy-of-clojure-lab.core
   (:use [clojure.test               :only [run-tests]])
-  (:use [midje.sweet])
-  (:use [clojure.contrib.repl-utils :only [show]])
-  (:use [clojure.pprint             :only [pprint]])
-  (:use [clojure.walk               :only [macroexpand-all]])
-  (:use clojure.contrib.math))
+  (:use [midje.sweet]))
+
+(defn map-bit ""
+  [max-x max-y]
+  (for [x (range max-x), y (range max-y)] [x y]))
+
+(fact (map-bit 2 2) => [[0 0] [0 1] [1 0] [1 1]] )
+
+(defn map-bit-xor ""
+  [max-x max-y] (map (fn [[x y]] [x y (bit-xor x y)]) (map-bit max-x max-y)))
+
+;.;. A clean boundary between useful abstractions and the grubby code that
+;.;. touches the real world is always a good thing. -- Ron Jeffries
+(fact (map-bit-xor 2 2) => [[0 0 0] [0 1 1] [1 0 1] [1 1 0]])
