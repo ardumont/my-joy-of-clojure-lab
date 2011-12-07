6599b5fac20465cbfd853305340135043d030a3d
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2011-12-07 19:31:06 +0100

    Add the current ns needed.

--- src/my_joy_of_clojure_lab/core.clj
+++ src/my_joy_of_clojure_lab/core.clj
@@ -1,1 +1,7 @@
-(ns my-joy-of-clojure-lab.core)
+(ns my-joy-of-clojure-lab.core
+  (:use [clojure.test               :only [run-tests]])
+  (:use [midje.sweet])
+  (:use [clojure.contrib.repl-utils :only [show]])
+  (:use [clojure.pprint             :only [pprint]])
+  (:use [clojure.walk               :only [macroexpand-all]])
+  (:use clojure.contrib.math))
