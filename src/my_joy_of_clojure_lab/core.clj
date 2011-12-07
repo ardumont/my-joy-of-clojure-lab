a393c1d4d7459edf34693e27477b313eae6c3f76
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2011-12-07 19:16:06 +0100

    Bootstrap project.

---
+++ src/my_joy_of_clojure_lab/core.clj
@@ -0,0 +1,1 @@
+(ns my-joy-of-clojure-lab.core)
---
+++ test/my_joy_of_clojure_lab/test/core.clj
@@ -0,0 +1,6 @@
+(ns my-joy-of-clojure-lab.test.core
+  (:use [my-joy-of-clojure-lab.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
