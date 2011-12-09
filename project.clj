89277f85cc2a3c234855b84eae6e050887a84de2
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2011-12-10 00:14:24 +0100

    Upgrade to midje 1.3.0-RC4.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject my-joy-of-clojure-lab "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]]
-  :dev-dependencies [[midje "1.3-alpha5"]] )
+  :dev-dependencies [[midje "1.3.0-RC4"]] )
--- src/my_joy_of_clojure_lab/core.clj
+++ src/my_joy_of_clojure_lab/core.clj
@@ -2,10 +2,38 @@
   (:use [clojure.test               :only [run-tests]])
   (:use [midje.sweet]))
 
-(defn map-bit-xor ""
+;; xor
+
+(defn xors ""
   [max-x max-y] (for [x (range max-x), y (range max-y)] [x y (bit-xor x y)]))
 
-;.;. Out of clutter find simplicity; from discord find harmony; in the
-;.;.                               ; middle of difficulty lies opportunity.
-;.;.                               ; -- Einstein
-(fact (map-bit-xor 2 2) => [[0 0 0] [0 1 1] [1 0 1] [1 1 0]])
+;.;. The sum of wisdom is that time is never lost that is devoted to work.
+;.;. -- Emerson
+(fact (xors 2 2) => [[0 0 0] [0 1 1] [1 0 1] [1 1 0]])
+
+;; java method discovery
+
+;; instanciates a new frame
+(def frame (java.awt.Frame.))
+
+;; retrieve all the methods from the Frame class.
+;; (seq (.getMethods java.awt.Frame))
+
+(defn filter-method "Filter the methods from a java class"
+  [clazz re]
+  (for [method (seq (.getMethods clazz))
+        :let [method-name (.getName method)]
+        :when (re-find re method-name)]
+    method-name))
+
+(fact
+  (filter-method java.awt.Frame #"Vis") => ["setVisible", "isVisible"])
+
+(defn xors-frame ""
+  [frame f]
+  (let [gfx (.getGraphics frame)]
+#_    (do
+      (.setSize frame (java.awt.Dimension. 200 200))
+      (.fillRect gfx 100 100 50 75)
+      (.setColor gfx (java.awt.Color. 255 128 0))
+      (.fillRect gfx 100 150 75 50))))
